e885dc1a87542aa9a5598190d5e223d39c2a12ef
takipone <takipone@gmail.com>
2021-12-13 09:13:24 +0900

    fix form margin

--- src/components/travelnoteform.js
+++ src/components/travelnoteform.js
@@ -1,13 +1,15 @@
-import { Input, Textarea, Button } from "@chakra-ui/react";
+import { VStack, Flex, Heading, Input, Textarea, Button } from "@chakra-ui/react";
 
 export const TravelNoteForm = ({
     handleAddTravelNote
 }) => {
     return (
-        <>
+        <VStack spacing={1} p="1">
+            <Heading as="h2" size='md' mt="16px">旅人のこえ</Heading>
             <Input
                 placeholder="お名前"
                 mt="2"
+                m="1"
             />
             <Input
                 placeholder="SNSのID"
@@ -17,20 +19,22 @@ export const TravelNoteForm = ({
                 placeholder="旅の感想を入力ください"
                 mt="2"
             />
-            <Button
-                m="4"
-                mr="0"
-                colorScheme="teal"
-                onClick={handleAddTravelNote}
-            >
-                お墨付き投稿
-            </Button>
-            <Button
-                m="4"
-                onClick={handleAddTravelNote}
-            >
-                投稿
-            </Button>
-        </>
+            <Flex>
+                <Button
+                    m="4"
+                    mr="0"
+                    colorScheme="teal"
+                    onClick={handleAddTravelNote}
+                >
+                    お墨付き投稿
+                </Button>
+                <Button
+                    m="4"
+                    onClick={handleAddTravelNote}
+                >
+                    投稿
+                </Button>
+            </Flex>
+        </VStack>
     )
 };
