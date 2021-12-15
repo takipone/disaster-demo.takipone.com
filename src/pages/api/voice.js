2e65df9b1c2dbe6aa3a049614da472004a71e073
takipone <takipone@gmail.com>
2021-12-15 14:09:40 +0900

    fix ref

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
 //    if (req.method === 'POST') {
-        const crlPop = req.headers.x-crl-pop;
-        const crlToken = req.headers.x-crl-token;
+        const crlPop = req.headers['x-crl-pop'];
+        const crlToken = req.headers['x-crl-token'];
         res.status(200).json({
             pop: crlPop,
             token: crlToken
