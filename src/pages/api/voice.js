652ecc208888fc02c54355c67b803af34dbd3fd8
takipone <takipone@gmail.com>
2021-12-15 13:59:58 +0900

    disable method selection

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -1,13 +1,13 @@
 export default function handler(req, res) {
-    if (req.method === 'POST') {
+//    if (req.method === 'POST') {
         const crl_pop = req.get('X-Crl-Pop');
         const crl_token = req.get('X-Crl-Token');
         res.status(200).json({
             pop: crl_pop,
             token: crl_token
         });
-    } else {
-        res.status(405).send('method not allowed');
-    }
+//    } else {
+//        res.status(405).send('method not allowed');
+//    }
 
 }
