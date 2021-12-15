27e58d3bb1d2196362246b0bf2d9209df7679398
takipone <takipone@gmail.com>
2021-12-15 13:57:22 +0900

    return crl pop/token

--- src/pages/api/voice.js
+++ src/pages/api/voice.js
@@ -1,3 +1,13 @@
 export default function handler(req, res) {
-    res.status(200).send('ok');
+    if (req.method === 'POST') {
+        const crl_pop = req.get('X-Crl-Pop');
+        const crl_token = req.get('X-Crl-Token');
+        res.status(200).json({
+            pop: crl_pop,
+            token: crl_token
+        });
+    } else {
+        res.status(405).send('method not allowed');
+    }
+
 }
